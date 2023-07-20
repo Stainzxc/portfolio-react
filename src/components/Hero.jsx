9610ba30bf1c91922c5032e19bc09dd908ee5f6c
Stainzxc <106868902+Stainzxc@users.noreply.github.com>
2023-07-20 14:13:45 +0800

    updated-portfolio

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HiArrowNarrowRight } from "react-icons/hi"
+import { HiArrowNarrowRight } from "react-icons/hi";
 
 export default function Hero() {
   return (
@@ -7,19 +7,24 @@ export default function Hero() {
       {/* container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-[#66FCF1] text-2xl sm:text-4xl">Hello, my name is</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Ruel Almonia</h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#45A29E]">I'm a Front-End Developer</h2>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Ruel Almonia
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#45A29E]">
+          I'm a Web Developer
+        </h2>
         <p className="text-[#C5C6C7] py-4 max-w-[700px] text-xl">
           Front-end developer with a passion for building interesting and
           user-friendly web interfaces. Highly driven and committed. Have the
           eagerness to learn more, especially in web development.
         </p>
         <div>
-            <button className="text-white group border-2 px-6 py-3 my-2 border-cyan-600 hover:bg-cyan-600 hover:border-cyan-600 flex items-center" >View Work 
+          <button className="text-white group border-2 px-6 py-3 my-2 border-cyan-600 hover:bg-cyan-600 hover:border-cyan-600 flex items-center">
+            View Work
             <span className="group-hover:rotate-90 duration-300">
-            <HiArrowNarrowRight className="ml-3"/>
+              <HiArrowNarrowRight className="ml-3" />
             </span>
-            </button>
+          </button>
         </div>
       </div>
     </div>
